4ef410556864cd7872a1107645ff1fc8f3bfb40e
kavyatc <kavyan@atconline.biz>
2018-03-29 09:51:19 +0530

    New setup of accouting

---
+++ resources/views/xxx.php
@@ -0,0 +1,10 @@
+[
+              'route' => '',
+              'command' => 'trial_balance',
+              'color' => 'purple',
+            ],
+            [
+              'route' => '',
+              'command' => 'balance_sheet',
+              'color' => 'orange',
+            ],
